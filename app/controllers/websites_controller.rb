9e1c007dfc526afdec6071e705de0a4fcbcbcb99
youichi yamazaki <denncoru@yahoo.co.jp>
2019-10-10 11:34:27 +0900

    トップページを作成

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,8 +1,21 @@
 class WebsitesController < ApplicationController
   def index
+    @websites = Website.all
   end
 
   def new
-    Website.new
+    @website = Website.new
   end
+
+  def create
+    Website.create(website_params)
+    redirect_to new_website_path
+  end
+
+  private
+
+  def website_params
+    params.require(:website).permit(:content)
+  end
+
 end
